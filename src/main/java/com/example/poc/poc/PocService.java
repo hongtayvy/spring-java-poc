92c307ed314e7ba541ff5f2482933ec8636d95fd
Victor Yang <victoryang@Victors-M1Pro-3.local>
2023-12-10 23:19:54 -0600

    chore: added di

--- src/main/java/com/example/poc/poc/PocService.java
+++ src/main/java/com/example/poc/poc/PocService.java
@@ -3,7 +3,10 @@ package com.example.poc.poc;
 import com.example.poc.common.client.GenshinClient;
 import com.example.poc.poc.dto.TypesDTO;
 import com.example.poc.poc.mapper.PocMapper;
+import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
+
+@Service
 public class PocService {
     private final GenshinClient genshinClient = new GenshinClient();
     private final PocMapper pocMapper = new PocMapper();
