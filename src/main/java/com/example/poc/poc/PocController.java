7ed0d7c621604b96f19f1962fdd8ff6ec97fcf56
Victor Yang <victoryang@Victors-M1Pro-3.local>
2023-12-10 23:15:28 -0600

    chore: clean up api

--- src/main/java/com/example/poc/poc/PocController.java
+++ src/main/java/com/example/poc/poc/PocController.java
@@ -1,12 +1,10 @@
 package com.example.poc.poc;
 
 import com.example.poc.poc.dto.TypesDTO;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
 
-@Controller
 @RestController("value = /poc")
 public class PocController {
     private final PocService pocService = new PocService();
